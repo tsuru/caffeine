76fd7d1d8e73022c66b3486e2cb59569e6c1840d
Guilherme Garnier <guilherme.garnier@gmail.com>
2015-12-02 11:52:44 -0200

    Test startApp function

--- app_test.go
+++ app_test.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"net/http"
+	"net/http/httptest"
+	"os"
 	"testing"
 
 	"gopkg.in/check.v1"
@@ -12,6 +14,21 @@ func Test(t *testing.T) { check.TestingT(t) }
 type AppSuite struct{}
 
 var _ = check.Suite(&AppSuite{})
+
+func (s *AppSuite) TestStartApp(c *check.C) {
+	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.Assert(r.Method, check.Equals, "POST")
+		c.Assert(r.URL.String(), check.Equals, "/apps/myapp/start")
+		c.Assert(r.Header.Get("Authorization"), check.Equals, "bearer 123")
+	}))
+	defer ts.Close()
+	os.Setenv("TSURU_HOST", ts.URL)
+	os.Setenv("TOKEN", "123")
+
+	req, err := http.NewRequest("GET", "http://myapp.mytsuru.com", nil)
+	c.Assert(err, check.IsNil)
+	startApp(req)
+}
 
 func (s *AppSuite) TestAppNameCaffeine(c *check.C) {
 	req, err := http.NewRequest("GET", "http://tsuru-caffeine-proxy.mytsuru.com", nil)
