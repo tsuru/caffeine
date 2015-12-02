91ca6e03df41203aad0df72224f07b5514bde5a0
Andrews Medina <andrewsmedina@gmail.com>
2015-12-02 10:50:33 -0200

    split main in main and app and remove unused functions

---
+++ app.go
@@ -0,0 +1,43 @@
+package main
+
+import (
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"os"
+	"strings"
+)
+
+func startApp(r *http.Request) {
+	app, err := appName(r)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	log.Printf("app name: %s\n", app)
+
+	startAppUrl := fmt.Sprintf("http://%s/apps/%s/start", app, os.Getenv("TSURU_HOST"))
+	authToken := fmt.Sprintf("bearer %s", os.Getenv("TOKEN"))
+
+	client := &http.Client{}
+	req, _ := http.NewRequest("POST", startAppUrl, nil)
+	req.Header.Add("Authorization", authToken)
+	resp, _ := client.Do(req)
+	if resp.StatusCode != 200 {
+		log.Printf("Error trying to start app %s", app)
+		return
+	}
+
+	log.Printf("app %s started", app)
+}
+
+func appName(r *http.Request) (string, error) {
+	app := strings.Split(r.Host, ".")[0]
+	log.Printf("appName -> %s", app)
+	if app != "tsuru-caffeine-proxy" {
+		return app, nil
+	}
+
+	return "", errors.New("invalid app name")
+}
--- main.go
+++ main.go
@@ -1,33 +1,17 @@
 package main
 
 import (
-	"encoding/json"
-	"errors"
-	"fmt"
-	"io/ioutil"
 	"log"
 	"net/http"
 	"net/http/httputil"
-	"net/url"
 	"os"
 	"strconv"
-	"strings"
 	"time"
 
 	"github.com/garyburd/redigo/redis"
 )
 
 const HIPACHE_PREFIX = "frontend:"
-
-type App struct {
-	Units []Unit
-}
-
-type Unit struct {
-	ProcessName string
-	AppName     string
-	Address     *url.URL
-}
 
 var (
 	maxConn, _ = strconv.Atoi(os.Getenv("REDIS_MAX_CONN"))
@@ -61,71 +45,6 @@ func main() {
 	http.ListenAndServe("0.0.0.0:8888", nil)
 }
 
-func startApp(r *http.Request) {
-	app, err := appName(r)
-	if err != nil {
-		log.Println(err)
-		return
-	}
-	log.Printf("app name: %s\n", app)
-
-	startAppUrl := fmt.Sprintf("http://%s/apps/%s/start", app, os.Getenv("TSURU_HOST"))
-	authToken := fmt.Sprintf("bearer %s", os.Getenv("TOKEN"))
-
-	client := &http.Client{}
-	req, _ := http.NewRequest("POST", startAppUrl, nil)
-	req.Header.Add("Authorization", authToken)
-	resp, _ := client.Do(req)
-	if resp.StatusCode != 200 {
-		log.Printf("Error trying to start app %s", app)
-		return
-	}
-
-	log.Printf("app %s started", app)
-}
-
-func getAppUnits(request *http.Request) []Unit {
-	a, err := appName(request)
-	if err != nil {
-		log.Fatalf("Error: %s", err)
-		return nil
-	}
-	client := &http.Client{}
-	apiUrl := fmt.Sprintf("http://%s/apps/%s", a, os.Getenv("TSURU_HOST"))
-	req, _ := http.NewRequest("GET", apiUrl, nil)
-	authToken := fmt.Sprintf("bearer %s", os.Getenv("TOKEN"))
-	req.Header.Add("Authorization", authToken)
-	resp, _ := client.Do(req)
-
-	defer resp.Body.Close()
-	if resp.StatusCode != 200 {
-		log.Println("Error retrieving" + a + " api data")
-		return nil
-	}
-	data, err := ioutil.ReadAll(resp.Body)
-
-	if err != nil {
-		log.Fatalf("Error: %s", err)
-		return nil
-	}
-
-	var app App
-	err1 := json.Unmarshal(data, &app)
-	if err1 != nil {
-		log.Fatalf("Error: %s", err)
-		return nil
-	}
-
-	var units []Unit
-	for _, unit := range app.Units {
-		if unit.ProcessName == "web" {
-			fmt.Printf("%s -> %s\n", unit.AppName, unit.Address)
-			units = append(units, unit)
-		}
-	}
-	return units
-}
-
 func restoreRoute(r *http.Request) {
 	conn := redisPool.Get()
 	defer conn.Close()
@@ -136,14 +55,4 @@ func restoreRoute(r *http.Request) {
 	if err != nil {
 		log.Printf("Err: %s", err)
 	}
-}
-
-func appName(r *http.Request) (string, error) {
-	app := strings.Split(r.Host, ".")[0]
-	log.Printf("appName -> %s", app)
-	if app != "tsuru-caffeine-proxy" {
-		return app, nil
-	}
-
-	return "", errors.New("invalid app name")
 }
