047dd818a6679d815096ab3ac5511713b2f2dce6
Guilherme Garnier <guilherme.garnier@gmail.com>
2016-01-06 17:27:56 -0200

    Normalizing environment variable names

--- main.go
+++ main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"net/http"
 	"os"
@@ -13,14 +14,13 @@ import (
 const HIPACHE_PREFIX = "frontend:"
 
 var (
-	maxConn, _ = strconv.Atoi(os.Getenv("REDIS_MAX_CONN"))
-	redisPool  = redis.NewPool(func() (redis.Conn, error) {
-		c, err := redis.Dial("tcp", os.Getenv("REDIS_ADDR"))
+	redisPool = redis.NewPool(func() (redis.Conn, error) {
+		c, err := redis.Dial("tcp", hipacheRedisAddr())
 		if err != nil {
 			return nil, err
 		}
 		return c, err
-	}, maxConn)
+	}, hipacheRedisMaxConn())
 )
 
 func main() {
@@ -40,9 +40,31 @@ func main() {
 
 func restoreRoute(host string, conn redis.Conn) {
 	name := HIPACHE_PREFIX + host
-	log.Print("Deleting ", name)
+	log.Printf("Deleting %s\n", name)
 	_, err := conn.Do("LTRIM", name, 0, 0)
 	if err != nil {
-		log.Printf("Err: %s", err)
+		log.Printf("Err: %s\n", err)
 	}
+}
+
+func hipacheRedisAddr() string {
+	host := os.Getenv("HIPACHE_REDIS_HOST")
+	if host == "" {
+		host = "localhost"
+	}
+	port := os.Getenv("HIPACHE_REDIS_PORT")
+	if port == "" {
+		port = "6379"
+	}
+
+	return fmt.Sprintf("%s:%s", host, port)
+}
+
+func hipacheRedisMaxConn() int {
+	maxConnValue := os.Getenv("HIPACHE_REDIS_MAX_CONN")
+	if maxConnValue == "" {
+		maxConnValue = "10"
+	}
+	maxConn, _ := strconv.Atoi(maxConnValue)
+	return maxConn
 }
--- main_test.go
+++ main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+
 	"github.com/rafaeljusto/redigomock"
 	"gopkg.in/check.v1"
 )
@@ -10,4 +12,26 @@ func (s *Suite) TestRestoreRoute(c *check.C) {
 	host := "tsuru-caffeine-proxy.mytsuru.com"
 	conn.Command("LTRIM", HIPACHE_PREFIX+host, 0, 0).Expect("OK")
 	restoreRoute(host, conn)
+}
+
+func (s *Suite) TestHipacheRedisAddrWithDefaultConfiguration(c *check.C) {
+	os.Setenv("HIPACHE_REDIS_HOST", "")
+	os.Setenv("HIPACHE_REDIS_PORT", "")
+	c.Assert(hipacheRedisAddr(), check.Equals, "localhost:6379")
+}
+
+func (s *Suite) TestHipacheRedisAddrWithHostAndPort(c *check.C) {
+	os.Setenv("HIPACHE_REDIS_HOST", "192.168.50.4")
+	os.Setenv("HIPACHE_REDIS_PORT", "8989")
+	c.Assert(hipacheRedisAddr(), check.Equals, "192.168.50.4:8989")
+}
+
+func (s *Suite) TestHipacheRedisMaxConnDefaultConfiguration(c *check.C) {
+	os.Setenv("HIPACHE_REDIS_MAX_CONN", "")
+	c.Assert(hipacheRedisMaxConn(), check.Equals, 10)
+}
+
+func (s *Suite) TestHipacheRedisMaxConnConfiguredValue(c *check.C) {
+	os.Setenv("HIPACHE_REDIS_MAX_CONN", "50")
+	c.Assert(hipacheRedisMaxConn(), check.Equals, 50)
 }
