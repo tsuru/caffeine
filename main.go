b1e6261187d1afbd684c89dceeedccee2cb70d39
Paulo Alem <paulo.alem@corp.globo.com>
2016-01-13 13:27:25 -0200

    Remove tsuru's internal references

--- main.go
+++ main.go
@@ -1,26 +1,11 @@
 package main
 
 import (
-	"fmt"
 	"log"
 	"net/http"
 	"os"
 	"strconv"
 	"time"
-
-	"github.com/garyburd/redigo/redis"
-)
-
-const HIPACHE_PREFIX = "frontend:"
-
-var (
-	redisPool = redis.NewPool(func() (redis.Conn, error) {
-		c, err := redis.Dial("tcp", hipacheRedisAddr())
-		if err != nil {
-			return nil, err
-		}
-		return c, err
-	}, hipacheRedisMaxConn())
 )
 
 func main() {
@@ -31,58 +16,22 @@ func main() {
 			return
 		}
 
-		conn := redisPool.Get()
-		defer conn.Close()
-
-		restoreRoute(app.Ip, conn)
 		startApp(*app)
 
-		waitBeforeProxy()
+		waitBeforeProxy(nil)
 
 		proxy := createProxy(r)
 		proxy.ServeHTTP(w, r)
 	})
-
 	http.ListenAndServe("0.0.0.0:8888", nil)
-}
-
-func restoreRoute(appAddress string, conn redis.Conn) {
-	name := HIPACHE_PREFIX + appAddress
-	log.Printf("Deleting %s\n", name)
-	_, err := conn.Do("LTRIM", name, 0, 0)
-	if err != nil {
-		log.Printf("Error deleting route: %s\n", err)
-	}
-}
-
-func waitBeforeProxy() {
-	sleepTime, err := strconv.Atoi(getConfig("WAIT_BEFORE_PROXY"))
-	if err == nil && sleepTime > 0 {
-		log.Printf("Waiting %d seconds before proxying...\n", sleepTime)
-		time.Sleep(time.Duration(sleepTime) * time.Second)
-	}
-}
-
-func hipacheRedisAddr() string {
-	host := getConfig("HIPACHE_REDIS_HOST")
-	port := getConfig("HIPACHE_REDIS_PORT")
-
-	return fmt.Sprintf("%s:%s", host, port)
-}
-
-func hipacheRedisMaxConn() int {
-	maxConn, _ := strconv.Atoi(getConfig("HIPACHE_REDIS_MAX_CONN"))
-	return maxConn
 }
 
 func getConfig(key string) string {
 	defaultValues := map[string]string{
-		"HIPACHE_REDIS_HOST":     "localhost",
-		"HIPACHE_REDIS_PORT":     "6379",
-		"HIPACHE_REDIS_MAX_CONN": "10",
-		"TSURU_HOST":             "http://localhost",
-		"TSURU_APP_PROXY":        "",
-		"TSURU_TOKEN":            "",
+		"TSURU_HOST":        "http://localhost",
+		"TSURU_APP_PROXY":   "",
+		"TSURU_TOKEN":       "",
+		"WAIT_BEFORE_PROXY": "0",
 	}
 
 	value := os.Getenv(key)
@@ -91,4 +40,15 @@ func getConfig(key string) string {
 	}
 
 	return value
+}
+
+func waitBeforeProxy(sleep func(time.Duration)) {
+	if sleep == nil {
+		sleep = time.Sleep
+	}
+	sleepTime, err := strconv.Atoi(getConfig("WAIT_BEFORE_PROXY"))
+	if err == nil && sleepTime > 0 {
+		log.Printf("Waiting %d seconds before proxying...\n", sleepTime)
+		sleep(time.Duration(sleepTime) * time.Second)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -4,43 +4,16 @@ import (
 	"os"
 	"time"
 
-	"github.com/rafaeljusto/redigomock"
 	"gopkg.in/check.v1"
 )
 
-func (s *Suite) TestRestoreRoute(c *check.C) {
-	conn := redigomock.NewConn()
-	appAddress := "tsuru-caffeine-proxy.mytsuru.com"
-	conn.Command("LTRIM", HIPACHE_PREFIX+appAddress, 0, 0).Expect("OK")
-	restoreRoute(appAddress, conn)
-}
-
 func (s *Suite) TestWaitBeforeProxy(c *check.C) {
-	os.Setenv("WAIT_BEFORE_PROXY", "1")
-	start := time.Now()
-	waitBeforeProxy()
-	runTime := time.Since(start).Seconds()
-	c.Assert(runTime >= 1, check.Equals, true)
-}
-
-func (s *Suite) TestHipacheRedisAddrWithDefaultConfiguration(c *check.C) {
-	os.Setenv("HIPACHE_REDIS_HOST", "")
-	os.Setenv("HIPACHE_REDIS_PORT", "")
-	c.Assert(hipacheRedisAddr(), check.Equals, "localhost:6379")
-}
-
-func (s *Suite) TestHipacheRedisAddrWithHostAndPort(c *check.C) {
-	os.Setenv("HIPACHE_REDIS_HOST", "192.168.50.4")
-	os.Setenv("HIPACHE_REDIS_PORT", "8989")
-	c.Assert(hipacheRedisAddr(), check.Equals, "192.168.50.4:8989")
-}
-
-func (s *Suite) TestHipacheRedisMaxConnDefaultConfiguration(c *check.C) {
-	os.Setenv("HIPACHE_REDIS_MAX_CONN", "")
-	c.Assert(hipacheRedisMaxConn(), check.Equals, 10)
-}
-
-func (s *Suite) TestHipacheRedisMaxConnConfiguredValue(c *check.C) {
-	os.Setenv("HIPACHE_REDIS_MAX_CONN", "50")
-	c.Assert(hipacheRedisMaxConn(), check.Equals, 50)
+	did_wait := false
+	os.Setenv("WAIT_BEFORE_PROXY", "10")
+	waitBeforeProxy(func(duration time.Duration) {
+		expected := time.Duration(10) * time.Second
+		c.Assert(duration, check.Equals, expected)
+		did_wait = true
+	})
+	c.Assert(did_wait, check.Equals, true)
 }
